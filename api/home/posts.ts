c2f9b01cefaa4190a522d9bdf0af01c2bcc498a5
Kevin Huo <kevin.w.huo@gmail.com>
2023-01-29 00:26:37 -0500

    add api stubs

---
+++ api/home/posts.ts
@@ -0,0 +1,36 @@
+import {
+  useUser,
+  useSupabaseClient,
+  Session
+} from '@supabase/auth-helpers-react';
+
+import { Database } from '../types/supabase';
+type Users = Database['public']['Tables']['users']['Row'];
+
+export async function getPosts(setLoading: any, setData: any) {
+  const supabase = useSupabaseClient<Database>();
+  const user = useUser();
+
+  try {
+    setLoading(true);
+    if (!user) throw new Error('No user');
+
+    let { data, error, status } = await supabase
+      .from('posts')
+      .select(`title, description, is_ppv`)
+      .limit(10);
+
+    if (error && status !== 406) {
+      throw error;
+    }
+
+    if (data) {
+      setData(data);
+    }
+  } catch (error) {
+    alert('Error loading posts data!');
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
+}
