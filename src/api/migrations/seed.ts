3b69bc1f6cf3e9f20b42780e23577889415093ca
comp <compacc@comps-Air.home>
2023-01-30 07:26:10 -0800

    seeding data

--- src/api/migrations/seed.ts
+++ src/api/migrations/seed.ts
@@ -7,6 +7,15 @@ async function generateProfile() {
     display_name: 'Chris P. Bacon',
     bio: '',
     location: '',
+    profile_picture: 'www.image.com',
+    profile_banner_picture: 'www.imagebanner.com',
+    created_at: '2016-07-24T03:32:45.678Z'
+  });
+
+  const { error } = await supabaseClient.from('creators').insert({
+    id: 1,
+    user_id: '1dd94d8b-c048-4b21-8571-583296db317e',
+    is_verified: true,
     created_at: '2016-07-24T03:32:45.678Z'
   });
 
