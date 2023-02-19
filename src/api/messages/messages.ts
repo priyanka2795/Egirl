0b341d90bc553cefc06445f5d5f9ea67d6a21694
comp <compacc@comps-Air.home>
2023-02-18 19:40:25 -0800

    adding who is user field to messages, in addition with ascending order

--- src/api/messages/messages.ts
+++ src/api/messages/messages.ts
@@ -24,14 +24,61 @@ export async function getAllUserCharChat(
     client
   );
 
-  const messages = [...userMessages, ...characterMessages];
+  interface UserToCharacterMessage {
+    id: number;
+    sender_id: string;
+    recipient_id: number;
+    message: string;
+    created_at: string;
+  }
 
+  interface CharacterToUserMessage {
+    id: number;
+    sender_id: string;
+    recipient_id: number;
+    message: string;
+    created_at: string;
+  }
+
+  // adding who_is_sender field to user messages
+  const userMessagesWithSender = userMessages.map(
+    (message: UserToCharacterMessage) => ({
+      ...message,
+      who_is_sender:
+        typeof message.sender_id === 'number' ? 'character' : 'user'
+    })
+  );
+
+  // adding who_is_sender field to character messages
+  const characterMessagesWithSender = characterMessages.map(
+    (message: CharacterToUserMessage) => ({
+      ...message,
+      who_is_sender:
+        typeof message.sender_id === 'number' ? 'character' : 'user'
+    })
+  );
+
+  // merging and sorting messages by created_at field in ascending order
+  const messages = [...userMessagesWithSender, ...characterMessagesWithSender];
   messages.sort(
     (a, b) =>
       new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
   );
 
-  const arrMessages = messages.map((message) => message.message);
+  // extracting messages content with who_is_sender from messsages array
+  const arrMessages = messages.map((message) => ({
+    message: message.message,
+    who_is_sender: message.who_is_sender
+  }));
+
+  //   const messages = [...userMessages, ...characterMessages];
+
+  //   messages.sort(
+  //     (a, b) =>
+  //       new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+  //   );
+
+  //   const arrMessages = messages.map((message) => message.message);
   console.log(arrMessages);
   return arrMessages;
 }
@@ -62,20 +109,87 @@ export async function getLastTwentyUserCharChat(
     client
   );
 
-  // this uses the spread operator to concatenate the userMessages and characterMessages arrays
-  const messages = [...userMessages, ...characterMessages];
+  interface UserToCharacterMessage {
+    id: number;
+    sender_id: string;
+    recipient_id: number;
+    message: string;
+    created_at: string;
+  }
 
-  // sorting messages by created_at field in descending order
+  interface CharacterToUserMessage {
+    id: number;
+    sender_id: string;
+    recipient_id: number;
+    message: string;
+    created_at: string;
+  }
+
+  // adding who_is_sender field to user messages
+  const userMessagesWithSender = userMessages.map(
+    (message: UserToCharacterMessage) => ({
+      ...message,
+      who_is_sender:
+        typeof message.sender_id === 'number' ? 'character' : 'user'
+    })
+  );
+
+  // adding who_is_sender field to character messages
+  const characterMessagesWithSender = characterMessages.map(
+    (message: CharacterToUserMessage) => ({
+      ...message,
+      who_is_sender:
+        typeof message.sender_id === 'number' ? 'character' : 'user'
+    })
+  );
+
+  // merging and sorting messages by created_at field in ascending order
+  const messages = [...userMessagesWithSender, ...characterMessagesWithSender];
   messages.sort(
     (a, b) =>
       new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
   );
 
-  // extracting latest 20 messages content from messsages array
-  // const latestMessages = messages.slice(0, 20).map((message) => message.message);
-  const latestMessages = messages.slice(-20).map((message) => message.message);
+  // extracting latest 20 messages content with who_is_sender from messsages array
+  const latestMessages = messages.slice(0, 20).map((message) => ({
+    message: message.message,
+    who_is_sender: message.who_is_sender
+  }));
+
+  // -------------
+
+  //   // this uses the spread operator to concatenate the userMessages and characterMessages arrays
+  //   const messages = [...userMessages, ...characterMessages];
+  //     console.log(messages)
+
+  //   // sorting messages by created_at field in ascending order
+  //   messages.sort(
+  //     (a, b) =>
+  //       new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+  //   );
+
+  //   // extracting latest 20 messages content from messsages array
+  //   // const latestMessages = messages.slice(0, 20).map((message) => message.message);
+  //   const latestMessages = messages.slice(-20).map((message) => message.message);
+
+  // -------------
 
   console.log(latestMessages);
   return latestMessages;
 }
-// getLastTwentyUserCharChat('e8a2be37-76f6-4ebb-bfd8-b9e370046a41', 1, supabaseClient);
+getLastTwentyUserCharChat(
+  'e8a2be37-76f6-4ebb-bfd8-b9e370046a41',
+  1,
+  supabaseClient
+);
+
+// add who is the sender
+
+// {
+//     id: 1,
+//     sender_id: 1,
+//     recipient_id: 'e8a2be37-76f6-4ebb-bfd8-b9e370046a41',
+//     message: 'hey uwu uwu something something',
+//     who_is_sender: 'character'
+//     created_at: '2016-07-24T03:32:45.678'
+// }
