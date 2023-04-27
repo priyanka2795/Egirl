104ace0a33ba209d5b29190618e0542eae95d023
Holland Pleskac <hollandpleskac@gmail.com>
2023-04-27 09:46:42 -0700

    footer

--- src/components/landing-common/Footer.tsx
+++ src/components/landing-common/Footer.tsx
@@ -8,11 +8,11 @@ import ExperienceTheFuture from './assets/ExperienceTheFuture';
 
 const Footer = () => {
   return (
-    <div className='w-full bg-[#000000] px-24 lg:px-[100px] 2xl:px-[120px]'>
-      <div className='grid grid-cols-2 pt-[100px]'>
-        <div className=''>
+    <div className='w-full bg-[#000000] px-[40px] lg:px-[100px] 2xl:px-[120px]'>
+      <div className='grid grid-cols-1 pt-[64px] lg:grid-cols-2 lg:pt-[100px]'>
+        <div className='flex flex-col items-center lg:items-start'>
           <div className='flex justify-center lg:justify-start'>
-            <div className='mt-[103px] flex items-center rounded-[8px] bg-[#141414] px-[12px] py-[10px] '>
+            <div className='mt-0 flex items-center rounded-[8px] bg-[#141414] px-[12px] py-[10px] lg:mt-[103px] '>
               <ExperienceTheFuture className='mr-2' />
               <div className='justify-centertext-[32px] flex select-none items-center font-[500] text-white md:text-[24px] lg:text-[15px]'>
                 beta access
@@ -20,23 +20,23 @@ const Footer = () => {
             </div>
           </div>
           <div className='flex justify-center space-x-3 pt-6 text-center lg:justify-start lg:text-start'>
-            <span className='select-none text-[24px] font-[600] text-white lg:text-[48px]'>
+            <span className='select-none text-[24px] font-[600] text-white md:text-[48px]'>
               More than 3000 people have
               <span className='mx-2 font-[500] italic lg:mx-4'>
                 joined the waitlist!
               </span>
             </span>
           </div>
-          <button className='mt-12 rounded-xl bg-[#5848BC] py-4 px-[30px] text-lg font-semibold text-white transition duration-100 hover:bg-[#4D37DA]'>
+          <button className='mt-12 rounded-xl bg-[#5848BC] py-4 px-[40px] text-lg font-semibold text-white transition duration-100 hover:bg-[#4D37DA] lg:px-[30px]'>
             Join us
           </button>
         </div>
-        <div className='flex justify-end '>
+        <div className='hidden justify-end lg:flex '>
           <FooterImg />
         </div>
       </div>
-      <div className='mt-[75px] flex w-full justify-between border-b border-white/[.16]'>
-        <LogoWhite />
+      <div className='mt-[75px] flex w-full flex-col justify-between border-b border-white/[.16] lg:flex-row'>
+        <LogoWhite className='mb-[40px] lg:mb-0' />
         <div className='flex pb-[64px]'>
           <div>
             <div className='mr-[100px] select-none'>
@@ -52,28 +52,28 @@ const Footer = () => {
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                <DiscordIcon />
+                <DiscordIcon className='h-[24x] w-[24px]' />
               </a>
               <a
                 href='https://twitter.com/egirlsai'
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                <TwitterIcon />
+                <TwitterIcon className='h-[24x] w-[24px]' />
               </a>
               <a
                 href='https://www.instagram.com/egirls_ai/'
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                <InstaIcon />
+                <InstaIcon className='h-[24x] w-[24px]' />
               </a>
               <a
                 href='https://www.reddit.com/r/EgirlsAI/'
                 target='_blank'
                 rel='noopener noreferrer'
               >
-                <RedditIcon />
+                <RedditIcon className='h-[24x] w-[24px]' />
               </a>
             </div>
           </div>
