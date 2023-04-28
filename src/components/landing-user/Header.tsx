322004d76413de240649ce42c68f5ddc2358a25e
syahirAmali <syahiramali@gmail.com>
2023-04-28 11:14:18 +0800

    header links

--- src/components/landing-user/Header.tsx
+++ src/components/landing-user/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NextImage } from '@components/ui/next-image';
 import ExperienceTheFuture from '@components/landing-common/assets/ExperienceTheFuture';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className='flex w-full flex-col items-center bg-[#F6F6F6] px-[24px] pt-[188px] md:px-[64px] lg:flex-row lg:px-[100px] lg:pt-[96px] 2xl:px-[120px]'>
-      <div>
+      <div className='flex flex-col justify-center items-center lg:items-start w-full'>
         <div className='flex justify-center lg:justify-start'>
           <div className='flex  items-center rounded-[8px] bg-[#EAE8FD] px-[8px] py-[12px]'>
             <ExperienceTheFuture className='h-[40px] w-[40px] pr-[8px] lg:h-[20px] lg:w-[20px]' />
@@ -25,11 +26,13 @@ const Header = () => {
 
         <div className='flex justify-center pt-[20px] lg:justify-start'>
           <button className='mt-12 rounded-xl bg-[#5848BC] py-4 px-[30px] text-[16px] font-semibold text-white transition duration-100 hover:bg-[#4D37DA] md:text-lg'>
-            Join us
+            <Link href='https://discord.gg/ECAds8F8Dj'>
+              Join us
+            </Link>
           </button>
         </div>
       </div>
-      <div>
+      <div className='flex justify-center w-full'>
         <div className='flex shrink-0 items-center justify-center pt-[40px] pb-[74px] lg:py-[80px]'>
           <div className='flex shrink-0'>
             <NextImage
