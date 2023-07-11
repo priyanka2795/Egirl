d4dcdf68667b371f2154cd458371018cf649b6e0
Rishabh Bula <rishabhbula1712@gmail.com>
2023-07-11 17:09:52 +0530

    modified the image request modal design

--- src/components/messages/ImageRequestModal.tsx
+++ src/components/messages/ImageRequestModal.tsx
@@ -10,15 +10,42 @@ import gridImg5 from '../../../public/assets/messages/grid-img-10.png';
 import gridImg6 from '../../../public/assets/messages/grid-img-11.png';
 import gridImg7 from '../../../public/assets/messages/grid-img-12.png';
 import gridImg8 from '../../../public/assets/messages/grid-img-13.png';
+import gridImg9 from '../../../public/assets/messages/grid-img-14.png';
 import Image from 'next/image';
 
 type ImageRequestModal = {
   closeModal?: () => void;
 };
+
 const ImageRequestModal = ({ closeModal }: ImageRequestModal) => {
+  const imageFilterText = ['Clothes', 'Accessories', 'Poses', 'Locations'];
+  const requestedImageTypes = [
+    { image: gridImg3, title: 'Red Dress' },
+    { image: gridImg4, title: 'Some access...' },
+    { image: gridImg9, title: 'Red Dress' },
+    { image: gridImg3, title: 'Red Dress' }
+  ];
+
+  const imageType = [
+    { image: gridImg1, title: 'Red Dress' },
+    { image: gridImg2, title: 'Shorts' },
+    { image: gridImg3, title: 'Red Dress' },
+    { image: gridImg4, title: 'Red Dress' },
+    { image: gridImg5, title: 'Red Dress' },
+    { image: gridImg6, title: 'Red Dress' },
+    { image: gridImg7, title: 'Red Dress' },
+    { image: gridImg8, title: 'Red Dress' }
+  ];
   const [randomImageState, setRandomImageState] = useState(true);
+  const [imageRequestType, setImageRequestType] = useState(false);
+  const [imageRequestTab, setImageRequestTab] = useState('Clothes');
+  const handleImageRequestTab = (e: any) => {
+    setImageRequestTab(e.target.textContent);
+    console.log(e.target.textContent, 'get target');
+  };
+
   return (
-    <div className='flex h-[618px] w-[530px] flex-col items-center justify-start rounded-2xl bg-zinc-900'>
+    <div className='flex h-max w-[530px] flex-col items-center justify-start rounded-2xl bg-zinc-900'>
       <div className='inline-flex items-start justify-start gap-2 self-stretch border-b border-white border-opacity-10 p-6'>
         <div className='inline-flex shrink grow basis-0 flex-col items-start justify-start gap-1'>
           <div className='w-[286px] text-[18px] font-bold leading-normal text-white'>
@@ -38,125 +65,80 @@ const ImageRequestModal = ({ closeModal }: ImageRequestModal) => {
       <div className='flex h-[546px] flex-col items-start justify-start self-stretch'>
         <div className='flex h-[338px] flex-col items-start justify-start gap-5 self-stretch px-6 pb-5'>
           <div className='mt-6 flex gap-3'>
-            <button
-              className={`rounded-xl bg-white bg-opacity-20 px-4 py-2 font-bold text-[#979797] text-white`}
-            >
-              Clothes
-            </button>
-            <button
-              className={`rounded-xl px-4 py-2 font-bold text-[#979797] `}
-            >
-              Accessories
-            </button>
-            <button
-              className={`rounded-xl px-4 py-2 font-bold text-[#979797] `}
-            >
-              Poses
-            </button>
-            <button
-              className={`rounded-xl px-4 py-2 font-bold text-[#979797] `}
-            >
-              Locations
-            </button>
+            {imageFilterText.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  onClick={(e) => handleImageRequestTab(e)}
+                  className={`cursor-pointer rounded-xl px-4 py-2 font-bold text-[#979797] text-white ${
+                    imageRequestTab === item ? 'bg-white bg-opacity-20' : ''
+                  }`}
+                >
+                  {item}
+                </div>
+              );
+            })}
           </div>
 
           <div className='grid grid-cols-4 gap-[10px]'>
-            <div className='relative'>
-              <Image
-                src={gridImg1}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center  text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg2}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center  text-[13px] font-semibold'>
-                Shorts
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg3}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center  text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg4}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg5}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg6}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg7}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
-            <div className='relative'>
-              <Image
-                src={gridImg8}
-                alt=''
-                className='rounded-xl object-cover'
-              />
-              <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center text-[13px] font-semibold'>
-                Red Dress
-              </p>
-            </div>
+            {imageType.map((items, index) => {
+              return (
+                <div
+                  key={index}
+                  className='relative cursor-pointer'
+                  onClick={() => setImageRequestType(true)}
+                >
+                  <Image
+                    src={items.image}
+                    alt=''
+                    className='rounded-xl object-cover'
+                  />
+                  <p className='absolute bottom-[6px] left-0 h-max w-full bg-black/60 p-2 text-center  text-[13px] font-semibold'>
+                    {items.title}
+                  </p>
+                </div>
+              );
+            })}
           </div>
         </div>
-        <div className='flex h-28 flex-col items-start justify-start gap-2.5 self-stretch bg-neutral-900 px-6 pt-6'>
+        <div className='flex flex-col items-start justify-start gap-2.5 self-stretch bg-neutral-900 px-6 pt-6'>
           <div className='inline-flex items-start justify-start gap-3 self-stretch rounded-2xl bg-[#1A1A1A] p-5'>
             <div className='flex items-start justify-start gap-2.5 rounded-[100px] bg-white bg-opacity-5 p-3.5'>
               {/* <div className='relative w-5 h-5' /> */}
               <ImgIcon />
             </div>
             <div className='inline-flex shrink grow basis-0 flex-col items-start justify-start gap-3'>
-              <div className='flex h-[42px] flex-col items-start justify-start gap-1.5 self-stretch'>
+              <div className='flex flex-col items-start justify-start gap-1.5 self-stretch'>
                 <div className='self-stretch text-[14px] font-bold leading-none text-white'>
                   Image request
                 </div>
-                <div className='self-stretch text-[14px] font-normal leading-none text-neutral-400'>
-                  The selected styles will be displayed here.
-                </div>
+
+                {imageRequestType ? (
+                  <div className='mt-3 grid grid-cols-4 gap-[10px]'>
+                    {requestedImageTypes.map((items, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className='flex flex-col'
+                          onClick={() => setImageRequestType(!imageRequestType)}
+                        >
+                          <Image
+                            src={items.image}
+                            alt=''
+                            className='rounded-xl object-cover'
+                          />
+                          <p className='h-max w-full py-[6px] text-xs font-semibold'>
+                            {items.title}
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <div className='self-stretch text-[14px] font-normal leading-none text-neutral-400'>
+                    The selected styles will be displayed here.
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -169,7 +151,7 @@ const ImageRequestModal = ({ closeModal }: ImageRequestModal) => {
             Cancel
           </button>
           <button
-            className='rounded-2xl bg-indigo-700 px-5 py-[13px] text-[16px] font-bold leading-snug text-white'
+            className='rounded-2xl bg-[#5848BC] px-5 py-[13px] text-[16px] font-bold leading-snug text-white'
             onClick={closeModal}
           >
             Send request
