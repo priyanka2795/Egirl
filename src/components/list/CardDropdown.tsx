4a2fda6b13f1383dd5eeea3f024ac9c40d5f755f
Rishabh Bula <rishabhbula1712@gmail.com>
2023-07-19 17:57:02 +0530

    Created add collection modal and checkbox

--- src/components/list/CardDropdown.tsx
+++ src/components/list/CardDropdown.tsx
@@ -4,10 +4,12 @@ import PenIcon from '../../../public/assets/svgImages/pen-icon.svg';
 import DeleteIcon from '../../../public/assets/svgImages/delete-icon.svg';
 import EditCollectionModal from './EditCollectionModal';
 import DeleteCollection from './DeleteCollection';
+import CollectionCoverModal from './CollectionCoverModal';
 
 const CardDropdown = () => {
     const [editCollectionState, setEditCollectionState] = useState(false)
     const [deleteCollectionState , setDeleteCollectionState] = useState(false)
+    const [addCollectionState , setAddCollectionState] = useState(false)
   return (<>
     <div className="absolute right-2 top-[66px]">
     <div className='flex flex-col items-start justify-start w-[218px] h-max py-2 shadow rounded-2xl bg-[#1A1A1A] overflow-hidden'>
@@ -17,7 +19,7 @@ const CardDropdown = () => {
           Edit
         </div>
       </div>
-      <div className='flex items-center justify-start gap-2 self-stretch bg-[#1A1A1A] px-4 py-2.5  cursor-pointer'>
+      <div className='flex items-center justify-start gap-2 self-stretch bg-[#1A1A1A] px-4 py-2.5  cursor-pointer' onClick={() => setAddCollectionState(!addCollectionState)}>
         <AddIcon/>
         <div className='text-sm font-normal leading-none text-white shrink grow basis-0'>
           Add to collection
@@ -38,6 +40,9 @@ const CardDropdown = () => {
   {
     deleteCollectionState &&
     <DeleteCollection closeDeleteModal={setDeleteCollectionState} />
+  }
+  {addCollectionState &&
+    <CollectionCoverModal closeAddCollectionModal={setAddCollectionState}/>
   }
   </>
   )
--- src/components/list/CollectionCoverModal.tsx
+++ src/components/list/CollectionCoverModal.tsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import { Modal } from '@components/modal/modal';
+import Image from 'next/image';
+import React from 'react';
+import cameraIcon from '../../../public/assets/camera-icon.png';
+import avatar from '../../../public/assets/avatar.png';
+import CloseIcon from '../../../public/assets/svgImages/close-icon.svg';
+import MainImage from '../../../public/assets/gallery-tab-img-3.png';
 
-const CollectionCoverModal = () => {
-  return (
-    <div className="flex p-6 flex-cols">
-      <div></div>
-      <div></div>
-    </div>
-  )
+interface CollectionCoverModalProps {
+  closeAddCollectionModal?: any;
 }
 
-export default CollectionCoverModal
+const imagesArray = [MainImage , MainImage, MainImage , MainImage , MainImage , MainImage , MainImage, MainImage, MainImage, MainImage, MainImage, MainImage ,MainImage , MainImage , MainImage]
+const CollectionCoverModal = ({
+  closeAddCollectionModal
+}: CollectionCoverModalProps) => {
+  return (
+    <Modal
+      open={true}
+      modalClassName='flex flex-col w-full rounded-[20px] h-max bg-[#1A1A1A] max-w-[1004px]'
+      closeModal={() => closeAddCollectionModal(false)}
+      modalOverlayStyle='!bg-black/80'
+    >
+      <div>
+        <div className='flex w-full justify-between gap-2 border-b border-white/[0.08] p-6'>
+          <div className='flex w-[286px] flex-col items-start gap-1 text-lg font-bold text-[#FFFFFF]'>
+            Collection Cover
+          </div>
+          <div
+            className='top-[7px] flex h-[24px] w-[24px] cursor-pointer items-start gap-4 object-contain'
+            onClick={() => closeAddCollectionModal(false)}
+          >
+            <CloseIcon />
+          </div>
+        </div>
+
+        <div className='grid grid-cols-5 gap-3 px-6'>
+          {imagesArray.map((items , index) => {
+         return(
+          <div className='relative flex items-center'>
+            <div className="h-[196px] w-full">
+              <Image src={items} alt="" className="object-cover"/>
+            </div>
+          <div className='absolute custom-checkbox right-4 top-4'>
+            <input type='checkbox' id={`checked-${index}`} />
+            <label htmlFor={`checked-${index}`}></label>
+          </div>
+        </div>
+         )
+          })}
+       
+        </div>
+
+        <div className="flex items-end justify-end w-full gap-3 p-6 h-max">
+          <div className="flex items-center justify-center gap-2 px-5 py-3 border border-white cursor-pointer rounded-2xl border-opacity-30" onClick={() => closeAddCollectionModal(false)}>
+            <div className="text-base font-bold leading-snug text-white">Cancel</div>
+          </div>
+          <div className="flex items-center justify-center gap-2 px-5 py-3 bg-[#5848BC] rounded-2xl cursor-pointer" onClick={() => closeAddCollectionModal(false)}>
+            <div className="text-base font-bold leading-snug text-white">Create</div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default CollectionCoverModal;
