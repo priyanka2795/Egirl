615aec80c5539a3d71766320be8a3160d2335054
Rishabh Bula <rishabhbula1712@gmail.com>
2023-07-19 18:00:23 +0530

    modified the list tab condition

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,32 +1,41 @@
-import React,{useState} from 'react'
-import SubscriptionOptions from './SubscriptionOptions'
-import DefaultTab from '@components/common/DefaultTab'
-import CollectionOptions from './CollectionOptions'
-import RemoveProfile from './RemoveProfile'
-import EditCollectionModal from './EditCollectionModal'
+import React, { useState } from 'react';
+import SubscriptionOptions from './SubscriptionOptions';
+import DefaultTab from '@components/common/DefaultTab';
+import CollectionOptions from './CollectionOptions';
+import RemoveProfile from './RemoveProfile';
+import EditCollectionModal from './EditCollectionModal';
 
 const ListIndex = () => {
-  const [activeListTab, setActiveListTab] = useState("Subscriptions")
-  const [removePage, setRemovePage] = useState(false)
-  const tabContent = ['Subscriptions', 'Following' , 'Bookmarks' , 'Collections'];
+  const [activeListTab, setActiveListTab] = useState('Subscriptions');
+  const [removePage, setRemovePage] = useState(false);
+  const tabContent = ['Subscriptions', 'Following', 'Bookmarks', 'Collections'];
   return (
     <div>
-     
       <div onClick={() => setRemovePage(!removePage)}>Remove Page</div>
-      {removePage === true ?
-        <RemoveProfile/> : 
+      {removePage === true ? (
+        <RemoveProfile />
+      ) : (
         <>
-         <DefaultTab activeListTab={activeListTab} setActiveTab={setActiveListTab} tabContentArray={tabContent}/>
-      <div className="px-8 py-4">
-      {activeListTab !== "Collections" ?
-     <SubscriptionOptions/> : 
-      <CollectionOptions />
-     }   
-      </div>
+          <DefaultTab
+            activeListTab={activeListTab}
+            setActiveTab={setActiveListTab}
+            tabContentArray={tabContent}
+          />
+          <div className='px-8 py-4'>
+            {activeListTab === 'Subscriptions' ? (
+              <SubscriptionOptions />
+            ) : activeListTab === 'Collections' ? (
+              <CollectionOptions />
+            ) : activeListTab === 'Bookmarks' ? (
+              <></>
+            ) : (
+              <SubscriptionOptions />
+            )}
+          </div>
         </>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ListIndex
+export default ListIndex;
