4a79007971e040d0bd865a6ce2cc849237db28e8
Rishabh Bula <rishabhbula1712@gmail.com>
2023-07-25 18:45:28 +0530

    updated referral page

--- src/pages/referrals.tsx
+++ src/pages/referrals.tsx
@@ -1,17 +1,13 @@
 import ReferralsIndex from '@components/Referrals'
+import Layout from '@components/common/Layout'
 import Sidebar from '@components/common/Sidebar'
 import React from 'react'
 
 const referrals = () => {
   return (
-    <main className='mx-auto flex min-h-screen max-w-[1320px] '>
-    <div>
-      <Sidebar />
-    </div>
-    <div className='max-w-[1020px] flex-grow bg-main-background sm:ml-[88px] lg:min-w-[600px] xl:ml-[300px]'>
+  <Layout>
     <ReferralsIndex/>
-    </div>
-  </main>
+  </Layout>
     )
 }
 
