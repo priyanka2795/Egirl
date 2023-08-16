a9f240a1866e8b8259f43d85b2c6729b9f4e5da0
Rishabh Bula <rishabhbula1712@gmail.com>
2023-08-16 13:17:10 +0530

    created the sidebar for creator-studio

--- src/components/creator-studio/CreatorStudioSidebar.tsx
+++ src/components/creator-studio/CreatorStudioSidebar.tsx
@@ -1,9 +1,155 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import avtar from '../../../public/assets/avatar.png';
+import arrowDown from '../../../public/assets/arrow-down.png';
+// import ChartIcon from '../../../public/assets/Chart.svg';
+import SidebarMenuItem from '@components/common/Sidebar/SidebarMenuItem';
+import AnalyticsIcon from './svg/AnalyticsIcon';
+import ImageGeneratorIcon from './svg/ImageGeneratorIcon';
+import HomeActiveIcon from './svg/HomeActiveIcon';
+import PersonalityIcon from './svg/PersonalityIcon';
+import VoiceIcon from './svg/VoiceIcon';
+import GiftIcon from './svg/GiftIcon';
+import ViewStyleIcon from './svg/ViewStyleIcon';
+import MarketplaceIcon from './svg/MarketplaceIcon';
+import StyleGenerator from './svg/StyleGenerator';
+import MoreIcon from './svg/MoreIcon.svg';
+// const SideBarMenu = [
+//   {
+//     id: 1,
+//     icon: ChartIcon,
+//     text: 'Analytics'
+//   },
+//   {
+//     id: 2,
+//     heading: 'Images',
+//     subchild: [
+//       {
+//         id: 2.1,
+//         icon: '',
+//         text: 'View Images'
+//       },
+//       {
+//         id: 2.2,
+//         icon: '',
+//         text: 'Image Generator'
+//       }
+//     ]
+//   },
+//   {
+//     id: 3,
+//     heading: 'Chatbot',
+//     subchild: [{ id: 3.1, icon: '', text: 'View Images' },{id:3.2 , icon:'', text:'Voice'},{id:3.3 , icon:'', text:'Gifts'}]
+//   },
+//   {id:4,}
+// ];
 
 const CreatorStudioSidebar = () => {
   return (
-    <div className="max-w-[276px] w-full h-full">sidebar</div>
-  )
-}
+    <div className='h-full w-full max-w-[276px] flex flex-col justify-between'>
+      <div>
+      <div className='flex items-center justify-between py-[14px] pl-3 pr-4 cursor-pointer'>
+        <div className='flex items-center gap-2'>
+          <div className='h-[32px] w-[32px]'>
+            <Image src={avtar} alt='' className='object-cover' />
+          </div>
+          Mika-chan
+        </div>
+        <div className='h-full'>
+          <Image src={arrowDown} alt='' />
+        </div>
+      </div>
 
-export default CreatorStudioSidebar
+      <SidebarMenuItem
+        text='Analytics'
+        href='/home'
+        Icon={AnalyticsIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+      <div className='inline-flex h-10 w-[300px] items-start justify-start gap-2.5 px-6 py-2.5'>
+        <div className='text-[13px] font-semibold uppercase leading-tight tracking-tight text-neutral-600'>
+          Images
+        </div>
+      </div>
+      <SidebarMenuItem
+        text='View Images'
+        href='/home'
+        Icon={ImageGeneratorIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+      <SidebarMenuItem
+        text='Image Generator'
+        href='/home'
+        Icon={AnalyticsIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+
+      <div className='inline-flex h-10 w-[300px] items-start justify-start gap-2.5 px-6 py-2.5'>
+        <div className='text-[13px] font-semibold uppercase leading-tight tracking-tight text-neutral-600'>
+          Chatbot
+        </div>
+      </div>
+      <SidebarMenuItem
+        text='Personality'
+        href='/home'
+        Icon={PersonalityIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+      <SidebarMenuItem
+        text='Voice'
+        href='/home'
+        Icon={VoiceIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+      <SidebarMenuItem
+        text='Gifts'
+        href='/home'
+        Icon={GiftIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+      <div className='inline-flex h-10 w-[300px] items-start justify-start gap-2.5 px-6 py-2.5'>
+        <div className='text-[13px] font-semibold uppercase leading-tight tracking-tight text-neutral-600'>
+          Styles
+        </div>
+      </div>
+      <SidebarMenuItem
+        text='View Styles'
+        href='/home'
+        Icon={ViewStyleIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+         <SidebarMenuItem
+        text='Marketplace'
+        href='/home'
+        Icon={MarketplaceIcon}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+        <SidebarMenuItem
+        text='Style Generator'
+        href='/home'
+        Icon={StyleGenerator}
+        IconActive={HomeActiveIcon}
+        // sideBarMenuText={`${sideBarMenuText} `}
+      />
+      </div>
+      
+      <div className="flex items-center gap-2 py-3 pl-3 cursor-pointer">
+        <div>
+          <MoreIcon/>
+        </div>
+         <p className="text-white text-[15px] font-semibold">More</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default CreatorStudioSidebar;
---
+++ src/components/creator-studio/svg/ViewStyleIcon.tsx
@@ -0,0 +1,16 @@
+
+type SvgIconProps = {
+    svgClasses?: string;
+    strokeClasses?: string;
+  };
+  
+  export default function HomeIcon({ svgClasses, strokeClasses }: SvgIconProps) {
+    return (
+        <svg className={svgClasses} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path className={strokeClasses}  d="M12 2L2 7L12 12L22 7L12 2Z" stroke="#515151" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path className={strokeClasses} d="M2 17L12 22L22 17" stroke="#515151" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path className={strokeClasses} d="M2 12L12 17L22 12" stroke="#515151" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg> 
+        
+    );
+  }
