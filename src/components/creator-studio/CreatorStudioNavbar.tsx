eb93d3c5928093ce17cecc77bd1c6b512fb9ecb9
Rishabh Bula <rishabhbula1712@gmail.com>
2023-08-16 12:26:20 +0530

    worked on creator-studio

--- src/components/creator-studio/CreatorStudioNavbar.tsx
+++ src/components/creator-studio/CreatorStudioNavbar.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CreateStudioLogo from './svg/creator-studio-logo.svg';
 import MenuIcon from './svg/menu.svg';
 import UserImg from './svg/user-img.svg';
 import BellIcon from './svg/bell.svg';
+import NotificationModal from './NotificationModal';
 
 const CreatorStudioNavbar = () => {
+  const [notificationModal, setNotificationModal] = useState(false);
+
   return (
+    <>
     <div className='flex justify-between px-4 py-6 '>
       <div className="flex gap-5">
         <MenuIcon />
         <CreateStudioLogo />
       </div>
 
-      <div className="flex gap-6 justify-center items-center">
-        <BellIcon/>
+      <div className="flex items-center justify-center gap-6">
+        <BellIcon onClick={() => setNotificationModal(!notificationModal)}/>
         <UserImg/>
       </div>
     </div>
+    {
+      notificationModal && <NotificationModal />
+    }
+    </>
   );
 };
 
---
+++ src/components/creator-studio/NotificationModal.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const notification = [
+    {
+        name: 'Any Lee',
+        description: 'Generared style · 36 mins ago',
+        button: 'View Style'
+    },
+    {
+        name: 'Red Dress',
+        description: 'Generared style · 2 days ago',
+        button: 'View Style'
+    },
+    {
+        name: 'Silver Watch',
+        description: 'Generared style · 5 days ago',
+        button: 'View Style'
+    },
+];
+
+const NotificationModal = () => {
+    const [activeNotification, setActiveNotification] = useState('Any Lee');
+  return (
+    <div className='flex flex-col rounded-[14px] bg-[#1A1A1A] w-[484px]'>
+        <div className='flex justify-between px-6 pt-6 pb-5'>
+            <div className='text-[#FFFFFF] text-[18px] font-bold leading-6'>Notifications</div>
+            <div className='text-[#979797] text-[12px] font-bold leading-[18px] mt-[3px]'>Mark all as read</div>
+        </div>
+        {notification.map((item) => {
+            return(
+                <div className={`flex justify-between p-6 border-l ${activeNotification === item.name ? 'border-l border-[#5848BC] bg-[#5848BC]/[0.08]' : 'border-b border-white/[0.08]'}`} onClick={() => {setActiveNotification(item.name)}}>
+                    <div className='flex flex-col gap-[6px]'>
+                        <div className='text-[#FFFFFF] text-[18px] font-bold leading-6'>{item.name}</div>
+                        <div className='text-[#979797] text-[14px] font-normal leading-[18px]'>{item.description}</div>
+                    </div>
+                    <button className='flex px-4 py-[10px] justify-center items-center rounded-[12px] bg-white/[0.08] text-[#FFFFFF] text-[14px] font-bold leading-5'>{item.button}</button>
+                </div>
+            );
+        })}
+    </div>
+  )
+}
+
+export default NotificationModal;
