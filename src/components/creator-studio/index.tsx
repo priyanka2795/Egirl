c11fa04ef291634dad2cf518710ed74aae169bab
Rishabh Bula <rishabhbula1712@gmail.com>
2023-08-17 18:31:12 +0530

    Created the personality component

--- src/components/creator-studio/index.tsx
+++ src/components/creator-studio/index.tsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react'
 import CreatorStudioNavbar from './CreatorStudioNavbar'
 import CreatorStudioSidebar from './CreatorStudioSidebar'
 import CreatorStudioMainContent from './CreatorStudioMainContent'
+import Personality from './Personality'
+
 
 const CreatorStudio = () => {
   const [shrinkSideBar , setShrinkSideBar] = useState(false)
+  const [mainContent ,setMainContent] = useState();
   return (
     <div className="mx-auto max-w-[1320px] ">
     <CreatorStudioNavbar shrinkSideBar={shrinkSideBar} setShrinkSideBar={setShrinkSideBar}/>
     <div className="flex h-[calc(100vh-104px)] ">
         <CreatorStudioSidebar shrinkSideBar={shrinkSideBar} setShrinkSideBar={setShrinkSideBar}/>
         <CreatorStudioMainContent/>
+        {/* <Personality/> */}
     </div>
     </div>
   )
