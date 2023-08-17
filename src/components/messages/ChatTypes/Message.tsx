386d73278d7c00daa7683cb4ddb73c9aa6e5d75d
Rishabh Bula <rishabhbula1712@gmail.com>
2023-08-17 18:22:28 +0530

    worked on message section

--- src/components/messages/ChatTypes/Message.tsx
+++ src/components/messages/ChatTypes/Message.tsx
@@ -17,6 +17,8 @@ import MessageSlider from '../MessageSlider';
 import { Modal } from '@components/modal/modal';
 import ChatReactionEmoji from '../ChatReactionEmoji';
 import { Emoji } from 'emoji-picker-react';
+import FeedbackTexts from '../FeedbackTexts';
+import FeedbackSent from '../FeedbackSent';
 
 interface MessageProps {
   src: string;
@@ -54,16 +56,19 @@ const Message: React.FC<MessageProps> = ({
     slidesToShow: 1,
     slidesToScroll: 1
   };
-  // const handkeCloseModal = () => {};
+
   const [goldenSvg, setGoldenSvg] = useState(false);
   const [selectedStars, setSelectedStars] = useState(0);
   const [emojiPicker, setEmojiPicker] = useState(false);
   const [showSelectedEmojiList, setShowSelectedEmojiList] = useState(false);
   const [messageIdEmoji, setMessgeIdEmoji] = useState(0);
   const [showSelectedEmoji, setShowSeletedEmoji] = useState('');
+  const [showFeedText, setShowFeedText] = useState(false);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   const handleStarClick = (starCount: number) => {
     setSelectedStars(starCount);
+    setShowFeedText(true);
   };
 
   const handleStarHover = (starCount: number) => {
@@ -73,8 +78,18 @@ const Message: React.FC<MessageProps> = ({
   };
 
   return (
-      <div className={`flex py-4 refresh-icon-parent last:pb-0 ${chatName === 'Bubble chat' ? 'w-[52%]' : 'w-full'} ${chatName === 'Bubble chat' && name === 'You' ? 'justify-end items-end rounded-tl-[10px] rounded-tr-[10px] rounded-br-0 rounded-bl-[10px] bg-[#5848BC] ml-auto' : 'items-start w-full'}`}>
-        {chatName === 'Bubble chat' && name === 'You' ? '' : 
+    <div
+      className={`refresh-icon-parent flex py-4 last:pb-0 ${
+        chatName === 'Bubble chat' ? 'w-[52%]' : 'w-full'
+      } ${
+        chatName === 'Bubble chat' && name === 'You'
+          ? 'rounded-br-0 ml-auto items-end justify-end rounded-bl-[10px] rounded-tl-[10px] rounded-tr-[10px] bg-[#5848BC]'
+          : 'w-full items-start'
+      }`}
+    >
+      {chatName === 'Bubble chat' && name === 'You' ? (
+        ''
+      ) : (
         <Image
           key={0}
           src={src} // Change to your image path
@@ -82,163 +97,178 @@ const Message: React.FC<MessageProps> = ({
           width={40}
           height={40}
           className='rounded-full'
-        />}
-        <div className='flex flex-col w-full ml-3 group'>
-          <div className='mb-[2px] flex items-center'>
-            <span className='mr-2 text-[15px] font-medium leading-5 cursor-pointer'>
-              {name}
-            </span>
-            <span className='text-[12px] font-normal leading-4 text-[#979797]'>
-              {time}
-            </span>
-          </div>
-
-          {messageIcons ? (
-            <>
-              <div className='flex gap-[18px]'>
-                <span className={`refresh-icon text-[16px] font-normal leading-6 ${chatName === 'Bubble chat' ? 'w-[52%]' : 'w-full'} ${chatName === 'Bubble chat' && name === 'You' ? 'justify-end items-end rounded-tl-[10px] rounded-tr-[10px] rounded-br-0 rounded-bl-[10px] bg-[#5848BC] ml-auto' : 'items-start w-full'}`}>
-                  {message}
-              
-                </span>
-                <div className=''>
-                  <div className='flex gap-3 opacity-0 group-hover:flex group-hover:opacity-100 w-[78px] justify-end'>
-                    {regenerateIcon && <RefreshIcon className="cursor-pointer" />}
-                    <div
-                      onClick={() => {
-                        setShowSelectedEmojiList(!showSelectedEmojiList),
-                          setMessgeIdEmoji(messageId);
-                      }}
-                      className='relative'
-                    >
-                      {name !== 'You' && 
-                      <EmojiIcon className="cursor-pointer"/>
-                      }
-                      {showSelectedEmojiList === true &&
-                      messageIdEmoji === messageId ? (
-                        <ChatReactionEmoji
-                          setShowSeletedEmoji={setShowSeletedEmoji}
-                        />
-                      ) : null}
-                    </div>
-                    <CopyIcon className='h-5 cursor-pointer' />
-                  </div>
-                </div>
-              </div>
-              {showSelectedEmoji !== '' && (
-                <div className="rounded-full bg-neutral-800 px-[10px] py-[6px] w-max mt-2">
-                  <Emoji unified={showSelectedEmoji} size={12} />
-                </div>
-              )}
-            </>
-          ) : (
-            <span className='text-[16px] font-normal leading-6'>{message}</span>
-          )}
-
-          {gridImage === true ? (
-            <div className='w-full max-w-[324px]'>
-              <div className='grid grid-cols-2 gap-2 my-2'>
-                <Image className='object-cover' src={gridImgFirst} alt='' />
-                <Image className='object-cover' src={gridImgSecond} alt='' />
-              </div>
-              <div className='grid grid-cols-3 gap-2 my-2'>
-                <Image className='object-contain' src={gridImgThird} alt='' />
-                <Image className='object-contain' src={gridImgFourth} alt='' />
-                <Image className='object-contain' src={gridImgFifth} alt='' />
-              </div>
-            </div>
-          ) : (
-            ''
-          )}
-
-          {rateResponse ? (
-            <div className='flex items-center justify-between mt-3'>
-              {/* <div className='item-center flex gap-4 rounded-xl bg-[#1E1E1E] px-4 py-[5px]'> */}
-              {/* <p className='text-[13px] '>Rate this response</p> */}
-              <div
-                className='flex cursor-pointer rating'
-                // onClick={() => setGoldenSvg(!goldenSvg)}
-              >
-                <Link href='#'>
-                  <StarIcon
-                    className={`${selectedStars >= 1 ? 'goldenSvg' : ''}`}
-                    onMouseEnter={() => handleStarHover(1)}
-                    onClick={() => handleStarClick(1)}
-                  />
-                </Link>
-
-                <Link href='#'>
-                  <StarIcon
-                    className={`${selectedStars >= 2 ? 'goldenSvg' : ''}`}
-                    onMouseEnter={() => handleStarHover(2)}
-                    onClick={() => handleStarClick(2)}
-                  />
-                </Link>
-
-                <Link href='#'>
-                  <StarIcon
-                    className={`${selectedStars >= 3 ? 'goldenSvg' : ''}`}
-                    onMouseEnter={() => handleStarHover(3)}
-                    onClick={() => handleStarClick(3)}
-                  />
-                </Link>
-
-                <Link href='#'>
-                  <StarIcon
-                    className={`${selectedStars >= 4 ? 'goldenSvg' : ''}`}
-                    onMouseEnter={() => handleStarHover(4)}
-                    onClick={() => handleStarClick(4)}
-                  />
-                </Link>
-
-                <Link href='#'>
-                  <StarIcon
-                    className={`${selectedStars >= 5 ? 'goldenSvg' : ''}`}
-                    onMouseEnter={() => handleStarHover(5)}
-                    onClick={() => handleStarClick(5)}
-                  />
-                </Link>
-              </div>
-              {/* </div> */}
-              <div className='flex gap-2'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 20 20'
-                  fill='none'
-                >
-                  <path
-                    d='M12.5 5L7.5 10L12.5 15'
-                    stroke='#979797'
-                    stroke-width='1.8'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                  />
-                </svg>
-                <p className='text-[13px] font-semibold text-[#979797]'>2/2</p>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='20'
-                  height='20'
-                  viewBox='0 0 20 20'
-                  fill='none'
-                >
-                  <path
-                    d='M7.5 5L12.5 10L7.5 15'
-                    stroke='#979797'
-                    stroke-width='1.8'
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                  />
-                </svg>
-              </div>
-            </div>
-          ) : (
-            ''
-          )}
+        />
+      )}
+      <div className='flex flex-col w-full ml-3 group'>
+        <div className='mb-[2px] flex items-center'>
+          <span className='mr-2 cursor-pointer text-[15px] font-medium leading-5'>
+            {name}
+          </span>
+          <span className='text-[12px] font-normal leading-4 text-[#979797]'>
+            {time}
+          </span>
         </div>
-      </div>
 
+        {messageIcons ? (
+          <>
+            <div className='flex gap-[18px]'>
+              <span
+                className={`refresh-icon text-[16px] font-normal leading-6 ${
+                  chatName === 'Bubble chat' ? 'w-[52%]' : 'w-full'
+                } ${
+                  chatName === 'Bubble chat' && name === 'You'
+                    ? 'rounded-br-0 ml-auto items-end justify-end rounded-bl-[10px] rounded-tl-[10px] rounded-tr-[10px] bg-[#5848BC]'
+                    : 'w-full items-start'
+                }`}
+              >
+                {message}
+              </span>
+              <div className=''>
+                <div className='flex w-[78px] justify-end gap-3 opacity-0 group-hover:flex group-hover:opacity-100'>
+                  {regenerateIcon && <RefreshIcon className='cursor-pointer' />}
+                  <div
+                    onClick={() => {
+                      setShowSelectedEmojiList(!showSelectedEmojiList),
+                        setMessgeIdEmoji(messageId);
+                    }}
+                    className='relative'
+                  >
+                    {name !== 'You' && <EmojiIcon className='cursor-pointer' />}
+                    {showSelectedEmojiList === true &&
+                    messageIdEmoji === messageId ? (
+                      <ChatReactionEmoji
+                        setShowSeletedEmoji={setShowSeletedEmoji}
+                      />
+                    ) : null}
+                  </div>
+                  <CopyIcon className='h-5 cursor-pointer' />
+                </div>
+              </div>
+            </div>
+            {showSelectedEmoji !== '' && (
+              <div className='mt-2 w-max rounded-full bg-neutral-800 px-[10px] py-[6px]'>
+                <Emoji unified={showSelectedEmoji} size={12} />
+              </div>
+            )}
+          </>
+        ) : (
+          <span className='text-[16px] font-normal leading-6'>{message}</span>
+        )}
+
+        {gridImage === true ? (
+          <div className='w-full max-w-[324px]'>
+            <div className='grid grid-cols-2 gap-2 my-2'>
+              <Image className='object-cover' src={gridImgFirst} alt='' />
+              <Image className='object-cover' src={gridImgSecond} alt='' />
+            </div>
+            <div className='grid grid-cols-3 gap-2 my-2'>
+              <Image className='object-contain' src={gridImgThird} alt='' />
+              <Image className='object-contain' src={gridImgFourth} alt='' />
+              <Image className='object-contain' src={gridImgFifth} alt='' />
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+
+        {feedbackSent ? (
+          <FeedbackSent />
+        ) : showFeedText ? (
+          <FeedbackTexts
+            showFeedText={showFeedText}
+            setShowFeedText={setShowFeedText}
+            setFeedbackSent={setFeedbackSent}
+          />
+        ) : rateResponse ? (
+          <div className='flex items-center justify-between mt-3'>
+            {/* <div className='item-center flex gap-4 rounded-xl bg-[#1E1E1E] px-4 py-[5px]'> */}
+            {/* <p className='text-[13px] '>Rate this response</p> */}
+            <div
+              className='flex cursor-pointer rating'
+              // onClick={() => setGoldenSvg(!goldenSvg)}
+            >
+              <Link href='#'>
+                <StarIcon
+                  className={`${selectedStars >= 1 ? 'goldenSvg' : ''}`}
+                  onMouseEnter={() => handleStarHover(0)}
+                  onClick={() => handleStarClick(1)}
+                />
+              </Link>
+
+              <Link href='#'>
+                <StarIcon
+                  className={`${selectedStars >= 2 ? 'goldenSvg' : ''}`}
+                  onMouseEnter={() => handleStarHover(2)}
+                  onClick={() => handleStarClick(2)}
+                />
+              </Link>
+
+              <Link href='#'>
+                <StarIcon
+                  className={`${selectedStars >= 3 ? 'goldenSvg' : ''}`}
+                  onMouseEnter={() => handleStarHover(3)}
+                  onClick={() => handleStarClick(3)}
+                />
+              </Link>
+
+              <Link href='#'>
+                <StarIcon
+                  className={`${selectedStars >= 4 ? 'goldenSvg' : ''}`}
+                  onMouseEnter={() => handleStarHover(4)}
+                  onClick={() => handleStarClick(4)}
+                />
+              </Link>
+
+              <Link href='#'>
+                <StarIcon
+                  className={`${selectedStars >= 5 ? 'goldenSvg' : ''}`}
+                  onMouseEnter={() => handleStarHover(5)}
+                  onClick={() => handleStarClick(5)}
+                />
+              </Link>
+            </div>
+            {/* </div> */}
+            <div className='flex gap-2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='20'
+                height='20'
+                viewBox='0 0 20 20'
+                fill='none'
+              >
+                <path
+                  d='M12.5 5L7.5 10L12.5 15'
+                  stroke='#979797'
+                  stroke-width='1.8'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                />
+              </svg>
+              <p className='text-[13px] font-semibold text-[#979797]'>2/2</p>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='20'
+                height='20'
+                viewBox='0 0 20 20'
+                fill='none'
+              >
+                <path
+                  d='M7.5 5L12.5 10L7.5 15'
+                  stroke='#979797'
+                  stroke-width='1.8'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                />
+              </svg>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+
+        
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/messages/FeedbackModal.tsx
@@ -0,0 +1,50 @@
+import { Modal } from '@components/modal/modal';
+import Image from 'next/image';
+import React from 'react';
+import xMark from '../../../public/assets/xmark (1).png'
+
+interface FeedbackModalProps {
+    showFeedText: boolean;
+    setShowFeedText: any;
+    closeModal: any;
+    setFeedbackSent: any;
+}
+
+const FeedbackModal = ( {showFeedText, setShowFeedText, closeModal, setFeedbackSent} : FeedbackModalProps) => {
+
+    const handleCloseModal = () => {
+        closeModal(false);
+        setShowFeedText(false);
+    };
+
+    const handleSendButton = () => {
+        closeModal(false);
+        setFeedbackSent(true);
+    };
+
+  return (
+    <Modal 
+    open={true}
+    modalClassName='flex flex-col w-full rounded-[20px] h-max bg-[#1A1A1A] max-w-[400px]'
+    // closeModal={() => closeModalItem(false)}
+    closeModal={() => closeModal(false)}
+    modalOverlayStyle='!bg-black/80'
+  >
+    <div className='flex justify-between p-6 border-b border-white/[0.08]'>
+        <div className='text-[#FFFFFF] text-[18px] font-bold leading-6'>Thanks for your feedback</div>
+        <div className='w-6 h-6 cursor-pointer' onClick={handleCloseModal}>
+            <Image className='w-full h-full' src={xMark} alt={''} />
+        </div>
+    </div>
+    <div className='flex flex-col gap-6 px-8 py-6'>
+        <div className='flex flex-col gap-3'>
+            <div className='text-[#979797] text-[15px] font-normal leading-5'>Share your thoughts to help us improve:</div>
+            <div className='flex h-[100px] pl-4 pr-3 py-3 rounded-[14px] bg-white/[0.05] text-[#FFFFFF] text-[15px] font-normal leading-6'>The response seemed broken to me, it didn’t make sense</div>
+        </div>
+        <button className='flex px-5 py-[13px] justify-center items-center rounded-[14px] bg-[#5848BC] text-[#FFFFFF] text-[16px] font-bold leading-[22px]' onClick={handleSendButton}>Send</button>
+    </div>
+  </Modal>
+  )
+}
+
+export default FeedbackModal;
