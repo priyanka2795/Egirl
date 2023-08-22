0d51228da8940ffff5bc80fd0f6a89677370f71f
Rishabh Bula <rishabhbula1712@gmail.com>
2023-08-22 17:57:23 +0530

    updated the style for personality likes,traits and it's modal

--- src/components/common/CreatorStudioLayout.tsx
+++ src/components/common/CreatorStudioLayout.tsx
@@ -16,7 +16,7 @@ const CreatorStudioLayout = ({ children }: any) => {
             shrinkSideBar={shrinkSideBar}
             setShrinkSideBar={setShrinkSideBar}
           />
-        <div className='main-content-scroller relative h-[calc(100vh-104px)] flex-grow overflow-y-auto overflow-x-hidden bg-main-background lg:min-w-[600px]'>
+        <div className='main-content-scroller relative h-[calc(100vh-104px)] flex-grow overflow-y-auto overflow-x-hidden bg-main-background lg:min-w-[600px] pr-4'>
           {children}
           </div>
         </div>
--- src/components/creator-studio/PersonalityTraitsSection.tsx
+++ src/components/creator-studio/PersonalityTraitsSection.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Modal } from '@components/modal/modal';
+import InfoIcon from '../../../public/assets/svgImages/info-icon.svg';
+import CloseIcon from '../../../public/assets/svgImages/close-icon.svg';
 const PersonalityTraitsSection = () => {
-
-    const [openT, setOpenT] = React.useState(false);
+  const [openT, setOpenT] = React.useState(false);
   const handleOpenTraits = () => setOpenT(true);
   const handleCloseTraits = () => setOpenT(false);
 
-    const [selectedOptionsT, setSelectedOptionsT] = useState<string[]>([]);
+  const [selectedOptionsT, setSelectedOptionsT] = useState<string[]>([]);
 
   const handleOptionChangeT = (optionT: string) => {
     if (selectedOptionsT.includes(optionT)) {
@@ -20,33 +21,91 @@ const PersonalityTraitsSection = () => {
     setSelectedOptionsT(selectedOptionsT.filter((o) => o !== optionT));
   };
   return (
-    <div>
-      <div className='mx-2.5 my-3 mb-4 flex h-auto  max-w-full flex-col rounded-lg bg-zinc-900 '>
-          <div className='mx-2.5 my-3 mb-4 flex h-24 max-w-full rounded-lg bg-zinc-900 pl-6 pt-4 '>
-            <div className='mr-96'>
-              <h2 className='pb-2'>
-                <b className='text-lg'>Traits</b>
-              </h2>
-              <p className='text-stone-700'>0/10</p>
-            </div>
+    <>
+      <div className='flex h-auto w-full max-w-full flex-col rounded-lg bg-[#121212]'>
+        <div className='flex max-w-full p-6 pb-5 '>
+          <div className='w-full'>
+            <h2 className='flex gap-[6px] text-lg font-bold leading-[110%]'>
+              Traits <InfoIcon />{' '}
+            </h2>
 
-            <button
-              className='ml-96 ml-[27.4rem]  mt-2 h-[40px] w-[84px] justify-center rounded-xl border-2 border-zinc-50 text-sm font-bold'
-              onClick={handleOpenTraits}
-            >
-              <span>+ Add</span>
-            </button>
+            <p className='text-stone-700'>0/10</p>
           </div>
-          <div className='m-8 mt-0 flex flex-wrap gap-2'>
-            <div className='flex flex-wrap gap-5 '>
+          <button
+            className='flex h-[40px] w-[84px] items-center justify-center justify-center rounded-xl border border-white/[0.32] text-sm font-bold'
+            onClick={handleOpenTraits}
+          >
+            + Add
+          </button>
+        </div>
+        <div
+          className={`flex flex-wrap gap-2 ${
+            selectedOptionsT.length > 0 ? 'p-6 pt-0' : ''
+          }`}
+        >
+          <div className='flex flex-wrap gap-5 '>
+            {selectedOptionsT.map((optionT) => (
+              <div
+                key={optionT}
+                className='flex gap-2 pt-3 pb-3 pl-5 pr-5 text-sm rounded-xl bg-neutral-800'
+              >
+                {optionT}{' '}
+                <span
+                  className='cursor-pointer '
+                  onClick={() => handleRemoveOptionT(optionT)}
+                >
+                  {/* &#10006; */}
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='18'
+                    height='18'
+                    viewBox='0 0 18 18'
+                    fill='none'
+                  >
+                    <path
+                      d='M4.5 4.5L13.5 13.5M13.5 4.5L4.5 13.5'
+                      stroke='#979797'
+                      stroke-width='1.35'
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                    />
+                  </svg>
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* -------------------traits modal---------------------- */}
+
+      <Modal
+        open={openT}
+        closeModal={handleCloseTraits}
+        modalOverlayStyle='!bg-black/80 '
+        modalClassName={`bg-[#121212] flex shrink-0 flex-col w-[506px] rounded-2xl h-max max-w-[550px] relative rounded`}
+      >
+        <div className='flex items-center justify-between border-b border-white/[0.08] p-8 pb-6'>
+          <b className='text-2xl'>Traits</b>
+          <CloseIcon className='text-white' />
+        </div>
+
+        <div className='border-b border-white/[0.08] px-8 py-4'>
+          <input
+            className='mr-2 h-[48px] w-full rounded-[14px] border-none bg-[#1E1E1E] pl-[50px] text-[15px] font-light leading-6 text-[#979797] transition duration-100 focus:ring-1 focus:ring-[#5848BC]'
+            type='text'
+            placeholder='Search'
+          />
+          <div className={`flex-wrap gap-2 ${selectedOptionsT.length > 0 ? "flex pt-4": "hidden"}`}>
+            <div className='flex flex-wrap gap-2 '>
               {selectedOptionsT.map((optionT) => (
                 <div
                   key={optionT}
-                  className='flex gap-2 rounded-xl bg-neutral-800 pb-3 pl-5 pr-5 pt-3 text-sm'
+                  className='flex gap-2 p-1 py-[3px] px-2 text-sm rounded-lg bg-neutral-800'
                 >
                   {optionT}{' '}
                   <span
-                    className='cursor-pointer '
+                    className='cursor-pointer'
                     onClick={() => handleRemoveOptionT(optionT)}
                   >
                     {/* &#10006; */}
@@ -72,69 +131,12 @@ const PersonalityTraitsSection = () => {
           </div>
         </div>
 
-        {/* -------------------traits modal---------------------- */}
-
-      <Modal
-        open={openT}
-        closeModal={handleCloseTraits}
-        modalOverlayStyle='!bg-black/80 '
-        modalClassName={`bg-[#121212] flex shrink-0 flex-col gap-6 w-[506px] p-8 rounded-2xl h-max max-w-[550px] relative rounded`}
-      >
-        <b className='text-2xl'>Traits</b>
-        <hr className='mb-1 w-[100%] bg-zinc-900' />
-
-        <div>
-          
-        </div>
-
-        <input
-          className='py-auto mr-2 h-[48px] w-full rounded-[14px] border-none bg-[#1E1E1E] pl-[50px] text-[15px] font-light leading-6 text-[#979797] transition duration-100 focus:ring-1 focus:ring-[#5848BC]'
-          type='text'
-          placeholder='Search'
-        />
-
-        <div className='flex flex-wrap gap-2'>
-          <div className='flex flex-wrap gap-5 '>
-            {selectedOptionsT.map((optionT) => (
-              <div
-                key={optionT}
-                className='flex gap-2 rounded-lg   bg-neutral-800 p-1 pb-3 pl-5 pr-5 pt-3 text-sm'
-              >
-                {optionT}{' '}
-                <span
-                  className='cursor-pointer'
-                  onClick={() => handleRemoveOptionT(optionT)}
-                >
-                  {/* &#10006; */}
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='18'
-                    height='18'
-                    viewBox='0 0 18 18'
-                    fill='none'
-                  >
-                    <path
-                      d='M4.5 4.5L13.5 13.5M13.5 4.5L4.5 13.5'
-                      stroke='#979797'
-                      stroke-width='1.35'
-                      stroke-linecap='round'
-                      stroke-linejoin='round'
-                    />
-                  </svg>
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <hr className='mb-5 bg-zinc-900' />
-        <div className=' '>
+        {/* <hr className='mb-5 bg-zinc-900' /> */}
+        <div className='px-6 pt-4'>
           <div className=''>
-            <b>A</b>
-            <br />
-            
+          <div className="font-bold text-[#979797] pb-3">A</div>
+           
             <div className='flex flex-wrap gap-2'>
-             
-
               <div className='relative'>
                 <input
                   id='ta1'
@@ -229,16 +231,12 @@ const PersonalityTraitsSection = () => {
                   Angry
                 </label>
               </div>
-
-              
             </div>
           </div>
-          
 
           <div className=''>
-            <b>B</b>
-            <br />
-            
+          <div className="font-bold text-[#979797] pb-3">B</div>          
+
             <div className='flex flex-wrap gap-2'>
               <div className='relative'>
                 <input
@@ -281,9 +279,8 @@ const PersonalityTraitsSection = () => {
           </div>
 
           <div className=''>
-            <b>C</b>
-            <br />
-            
+          <div className="font-bold text-[#979797] pb-3">C</div>          
+
             <div className='flex flex-wrap gap-2'>
               <div className='relative'>
                 <input
@@ -402,30 +399,25 @@ const PersonalityTraitsSection = () => {
           </div>
         </div>
 
-        
+        <div className='relative mt-2'></div>
 
-          <div className='relative mt-2'>
-            
-          </div>
-          
-        
-        <div className='flex flex flex-row  gap-3 self-stretch pb-[10px] pl-[10px] pr-[10px] pt-[0px] '>
+        <div className='flex flex-row self-stretch gap-3 px-8 pt-4 pb-8'>
           <button
             onClick={handleCloseTraits}
-            className=' flex-[1 0 0] flex h-[48px] w-[100%] items-center justify-center gap-2 rounded-full bg-blue-500 bg-zinc-800 px-4 py-2 pb-[13] pl-[20] pr-[20] pt-[13] text-lg text-sm text-xl font-bold text-white'
+            className='flex h-[48px] w-[100%] items-center justify-center rounded-[14px] border border-white/[0.32] px-5 py-[13px] font-bold'
           >
             Cancel
           </button>
           <button
             onClick={handleCloseTraits}
-            className='flex-[1 0 0] flex h-[48px] w-[100%] items-center justify-center gap-2 rounded-full bg-blue-500 bg-zinc-800 px-4 py-2 pb-[13] pl-[20] pr-[20] pt-[13] text-lg text-sm text-xl font-bold text-white'
+            className='flex h-[48px] w-[100%] items-center justify-center rounded-[14px] border border-[#5848BC] px-5 py-[13px] font-bold bg-[#5848BC]'
           >
             Save
           </button>
         </div>
       </Modal>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default PersonalityTraitsSection
+export default PersonalityTraitsSection;
