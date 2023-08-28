5af024eee283fd02df52a1268d60012ac01ec22e
Rishabh Bula <rishabhbula1712@gmail.com>
2023-08-28 15:33:48 +0530

    creating the view-image page

--- src/components/creator-studio/viewImages/Index.tsx
+++ src/components/creator-studio/viewImages/Index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Index = () => {
+const ViewImagesIndex = () => {
   return (
-    <div>Index</div>
+    <div className="flex justify-between">
+      <div className="text-[22px] font-bold leading-8 text-[#FFFFFF]">View Images</div>
+      <button className="cursor-pointer flex items-center justify-center gap-[6px] rounded-[12px] bg-[#5848BC] px-4 py-[10px]">
+      Create
+      </button>
+    </div>
   )
 }
 
-export default Index
+export default ViewImagesIndex
