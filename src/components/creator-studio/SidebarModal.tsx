86ba02f27b38311de691ef04a7142c332b04f3d6
Rishabh Bula <rishabhbula1712@gmail.com>
2023-09-06 16:04:20 +0530

    updating the creator studio page

--- src/components/creator-studio/SidebarModal.tsx
+++ src/components/creator-studio/SidebarModal.tsx
@@ -25,9 +25,9 @@ const sidebarModal = [
 const SidebarModal = () => {
   const [activeProfile, setActiveProfile] = useState('Mika-chan');
   return (
-    <div className={`top-[163px] mt-[7px] flex h-max w-[260px] flex-col rounded-[14px] bg-[#1A1A1A] px-0 pb-3 pt-2 fixed z-10`}>
+    <div className={`top-[131px] mt-2 flex h-max w-[260px] flex-col rounded-[14px] bg-[#1A1A1A] px-0 pb-3 pt-2 fixed z-10 -ml-1`}>
       <div className='px-6 py-[14px]'>
-        <div className='flex gap-[10px]'>
+        <div className='flex gap-[10px] '>
           <div className='h-8 h-max w-8 items-center justify-center rounded-full bg-white/[0.08] p-2'>
             <div className='w-4 h-4 overflow-hidden rounded-full'>
               <Image className='w-full h-full' src={userProfileIcon} alt={''} />
@@ -44,14 +44,14 @@ const SidebarModal = () => {
             <div onClick={() => {setActiveProfile(item.name)}}>
                 {activeProfile === item.name ? 
                  <div className='flex px-4 py-[6px]'>
-                 <div className='flex justify-between w-full mt-[5px] pl-2 pr-[14px] py-2 rounded-full bg-white/[0.08]'>
+                 <div className='flex justify-between w-full mt-[5px] pl-2 pr-[14px] py-2 rounded-full bg-white/[0.08] items-center'>
                      <div className='flex gap-[10px]'>
                          <div className='w-8 h-8'>
                              <Image className='w-full h-full' src={avatart1} alt={''} />
                          </div>
-                         <div className='mt-[6px] text-[14px] font-normal leading-[18px] text-[#FFFFFF]'>Mika-chan</div>
+                         <div className='flex items-center text-[14px] font-normal leading-[18px] text-[#FFFFFF] '>Mika-chan</div>
                      </div>
-                     <div className='w-4 h-4 mt-[5px]'>
+                     <div className='w-4 h-4'>
                          <Image className='w-full h-full' src={check} alt={''} />
                      </div>
                  </div>
