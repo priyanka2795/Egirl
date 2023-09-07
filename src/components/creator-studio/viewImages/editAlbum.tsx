9a3f7281ee1606e9c444113896882bb9328d37ee
Rishabh Bula <rishabhbula1712@gmail.com>
2023-09-07 16:45:34 +0530

    view images ALbum chnage

--- src/components/creator-studio/viewImages/editAlbum.tsx
+++ src/components/creator-studio/viewImages/editAlbum.tsx
@@ -11,8 +11,15 @@ import imageSquare from '../../../../public/assets/image-square.png';
 import image from '../../../../public/assets/image.png';
 import undo from '../../../../public/assets/Undo.png';
 import deleteIcon from '../../../../public/assets/trash-blank-alt.png';
+import image2 from '../../../../public/assets/vi-image-2.png'
+import image3 from '../../../../public/assets/vi-image-3.png'
+import image4 from '../../../../public/assets/vi-image-4.png'
+import image5 from '../../../../public/assets/vi-image-5.png'
+import image6 from '../../../../public/assets/vi-image-6.png'
+import image7 from '../../../../public/assets/vi-image-7.png'
 
-const images = [
+
+const CoverImage = [
     {
         image: image,
         text: 'Make album cover'
@@ -34,6 +41,33 @@ const images = [
         text: 'Delete'
     },
 ];
+
+const images = [
+    {
+      image: image1
+    },
+    {
+      image: image2
+    },
+    {
+      image: image3
+    },
+    {
+      image: image4
+    },
+    {
+      image: image5
+    },
+    {
+      image: image6
+    },
+    {
+      image: image7
+    },
+    {
+      image: image6
+    },
+  ];
 
 interface EditAlbum {
     CloseModal: any;
@@ -61,9 +95,9 @@ const EditAlbum = ({ CloseModal }: EditAlbum) => {
                     </div>
                     <div className='p-6 overflow-y-auto h-[500px]'>
                         <div className='grid grid-cols-4 gap-2'>
-                            {Array(10).fill('2').map(() => (
+                            {images.map((items) => (
                                 <div className='w-[193px] h-[193px] sub-banner' onClick={() => setCoverImg(false)}>
-                                    <Image src={AlbumFirst} className='w-full h-full' />
+                                    <Image src={items.image} className='w-full h-full' />
                                 </div>
                             ))}
                         </div>
@@ -105,16 +139,16 @@ const EditAlbum = ({ CloseModal }: EditAlbum) => {
                             <p className='font-semibold pb-3 text-[15px]'>Images</p>
                             <div className='h-[500px] overflow-y-auto overflow-x-hidden'>
                                 <div className='grid grid-cols-3 gap-2 '>
-                                    {Array(8).fill('2').map((item, index) => (
+                                    {images.map((item, index) => (
                                         <div className='w-[240px] h-[190px] sub-banner  relative '>
-                                            <Image src={image1} className='' />
+                                            <Image src={item.image} className='' />
                                             <div className='p-1.5 bg-[#0000007A] rounded-full absolute top-2 right-2 cursor-pointer' onClick={() => EditAlbumImgToggle(index)}>
                                                 <Pencil />
                                             </div>
                                             {editAlbumImg === index &&
                                                 <div className='absolute top-12 right-3 z-50' >
                                                     <div className='flex flex-col w-[218px] rounded-[14px] bg-[#1A1A1A]'>
-                                                        {images.map((item, index) => {
+                                                        {CoverImage.map((item, index) => {
                                                             return (
                                                                 <div key={index} className={`cursor-pointer flex gap-2 px-4 py-[10px]`}>
                                                                     <Image src={item.image} alt={''} />
