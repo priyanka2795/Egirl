f51d38ca7ebe5dc7ae151de490916cf4db0523ff
Rishabh Bula <rishabhbula1712@gmail.com>
2023-10-05 11:41:27 +0530

    updated postInput page

--- src/components/list/PostInput.tsx
+++ src/components/list/PostInput.tsx
@@ -9,7 +9,7 @@ const PostInput = () => {
     <div className='flex justify-between mt-5 p-6 rounded-[14px] bg-[#121212]'>
       <div className='flex items-center gap-3'>
         <Image className='object-contain rounded-[100px]' src={avatar} alt={''} />
-        <input type='text' className='text-[#979797] text-[15px] font-normal leading-6 h-0 border-none bg-transparent focus:ring-0 placeholder:text-[#979797]' placeholder={`What's happening?`} />
+        <input type='text' className='text-[#979797] text-[15px] font-normal leading-6 h-0 border-none bg-transparent focus:ring-0 placeholder:text-[#979797] pl-0' placeholder={`What's happening?`} />
       </div>
       <div className='flex items-center gap-3'>
         <Image className='object-contain' src={camera} alt={''} />
