51a6b3830d337567f806d66f18f128996223f04b
RishabhBula <rbula02@gmail.com>
2023-10-23 19:21:29 +0530

    create image generator,style generator and voice page design

---
+++ src/pages/learning-center-imageGenerator.tsx
@@ -0,0 +1,15 @@
+import LearningCenterLayout from '@components/common/LearningCenterLayout';
+import ImageGeneratorIndex from '@components/learning-center/imageGenerator';
+
+
+import React from 'react';
+
+function LearningCenterImageGenerator() {
+  return (
+    <LearningCenterLayout>
+    <ImageGeneratorIndex/>
+    </LearningCenterLayout>
+  );
+}
+
+export default LearningCenterImageGenerator;
