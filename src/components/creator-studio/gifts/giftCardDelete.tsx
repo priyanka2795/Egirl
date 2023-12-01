91853c4c61c7d7317984fe56835f7b5d9471579d
Rishabh Bula <rishabhbula1712@gmail.com>
2023-12-01 19:53:36 +0530

    Delete Gifts Functionality

--- src/components/creator-studio/gifts/giftCardDelete.tsx
+++ src/components/creator-studio/gifts/giftCardDelete.tsx
@@ -8,8 +8,10 @@ interface DeletePopup {
   Heading: string;
   Content: string;
   Img: boolean;
+  DeleteGift?: any;
+  DeleteIndex?: any;
   DeleteAllGift?: any;
-  selectedGiftData?: any;
+  selectedGiftData?:any,
   DeleteBtnStep?: number;
   DeleteActionCategory?: any;
   CategoryActionIndex?: number;
@@ -41,10 +43,10 @@ function GiftCardDelete({
           <div className='flex items-center justify-between border-b border-[#FFFFFF14] p-6'>
             <h5 className='text-lg font-semibold'>{Heading}</h5>
             <div
-              className='h-6 w-6 cursor-pointer'
+              className='w-6 h-6 cursor-pointer'
               onClick={() => DeleteModal(false)}
             >
-              <Image className='h-full w-full' src={crossIcon} alt={''} />
+              <Image className='w-full h-full' src={crossIcon} alt={''} />
             </div>
           </div>
           <div className='p-6'>
@@ -54,7 +56,7 @@ function GiftCardDelete({
               ) : (
                 <div className='m-auto mb-4 max-h-[100px] max-w-[100px] overflow-hidden rounded-xl'>
                   <Image
-                    className='h-full w-full object-cover'
+                    className='object-cover w-full h-full'
                     src={DeleteGiftImage || ''}
                   />
                 </div>
@@ -65,7 +67,7 @@ function GiftCardDelete({
                 {GiftName ? 'gift?' : ''}
               </p>
             </div>
-            <div className='mt-6 grid grid-cols-2 gap-3 font-semibold text-white'>
+            <div className='grid grid-cols-2 gap-3 mt-6 font-semibold text-white'>
               <button
                 className='rounded-[14px] border border-[#FFFFFF52] px-5 py-3'
                 onClick={() => DeleteModal(false)}
